c01d5f97b7595728e385aac493c0e0ac99382824
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-04 21:16:25 +0300

    .

---
+++ Services/Catalog/E-Commerce.Services.Catalog/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Services.Catalog.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Services/Catalog/E-Commerce.Services.Catalog/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Services.Catalog.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+    }
+}
