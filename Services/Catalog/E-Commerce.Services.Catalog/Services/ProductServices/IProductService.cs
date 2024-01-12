57c8ce23556f45e8646573f74b338ddbb17e7f97
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-12 20:13:31 +0300

    ..

--- Services/Catalog/E-Commerce.Services.Catalog/Services/ProductServices/IProductService.cs
+++ Services/Catalog/E-Commerce.Services.Catalog/Services/ProductServices/IProductService.cs
@@ -1,6 +1,15 @@
-﻿namespace E_Commerce.Services.Catalog.Services.ProductServices
+﻿using E_Commerce.Services.Catalog.Dtos.ProductDtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Services.Catalog.Services.ProductServices
 {
     public interface IProductService
     {
+        Task<List<ResultProductDto>> GetAllProductAsync();
+        Task<ResultProductDto> GetProductAsync(string id);
+        Task CreateProductAsync(CreateProductDto createProductDto);
+        Task UpdateProductAsync(UpdateProductDto updateProductDto);
+        Task DeleteProductAsync(string id);
     }
 }
