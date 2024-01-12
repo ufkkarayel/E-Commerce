2aa98447cf40d981c625e214872d7b7faf1a60b3
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-12 20:28:07 +0300

    ..

--- Services/Catalog/E-Commerce.Services.Catalog/Services/CategoryServices/CategoryService.cs
+++ Services/Catalog/E-Commerce.Services.Catalog/Services/CategoryServices/CategoryService.cs
@@ -1,6 +1,53 @@
-﻿namespace E_Commerce.Services.Catalog.Services.CategoryServices
+﻿using AutoMapper;
+using E_Commerce.Services.Catalog.Dtos.CategoryDtos;
+using E_Commerce.Services.Catalog.Models;
+using E_Commerce.Services.Catalog.Settings;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Services.Catalog.Services.CategoryServices
 {
-    public class CategoryService
+
+    public class CategoryService : ICategoryService
     {
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+        public CategoryService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
+        public async Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            var values = _mapper.Map<Category>(createCategoryDto);
+            await _categoryCollection.InsertOneAsync(values);
+        }
+
+        public async Task DeleteCategoryAsync(string id)
+        {
+            var values = await _categoryCollection.DeleteOneAsync(id);
+
+        }
+
+        public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            var values = await _categoryCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultCategoryDto>>(values);
+        }
+
+        public async Task<ResultCategoryDto> GetCategoryById(string id)
+        {
+            var values = await _categoryCollection.Find<Category>(x => x.CategoryID == id).FirstOrDefaultAsync();
+            return _mapper.Map<ResultCategoryDto>(values);
+        }
+
+        public async void UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            var values = _mapper.Map<Category>(updateCategoryDto);
+            await _categoryCollection.FindOneAndReplaceAsync(x => x.CategoryID == updateCategoryDto.CategoryID, values);
+        }
     }
 }
