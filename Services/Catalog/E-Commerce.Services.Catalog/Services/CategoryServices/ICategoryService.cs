a2b5a028b48c8d054844e28604998d9662113260
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-12 20:10:34 +0300

    ..

--- Services/Catalog/E-Commerce.Services.Catalog/Services/CategoryServices/ICategoryService.cs
+++ Services/Catalog/E-Commerce.Services.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -1,6 +1,16 @@
-﻿namespace E_Commerce.Services.Catalog.Services.CategoryServices
+﻿using E_Commerce.Services.Catalog.Dtos.CategoryDtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Services.Catalog.Services.CategoryServices
 {
     public interface ICategoryService
     {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        void CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        void UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        void DeleteCategoryAsync(string id);
+        ResultCategoryDto GetCategoryById(string id);
+
     }
 }
